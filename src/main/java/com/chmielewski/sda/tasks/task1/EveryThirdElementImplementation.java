d6a2351ddfa3e7dfa97f635712eef854264228f7
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-05 21:56:06 +0100

    Task1: added missing @Override annotation

--- src/main/java/com/chmielewski/sda/tasks/task1/EveryThirdElementImplementation.java
+++ src/main/java/com/chmielewski/sda/tasks/task1/EveryThirdElementImplementation.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EveryThirdElementImplementation<T> implements EveryThirdElement<T> {
+    @Override
     public List<T> getEveryThirdElement(List<T> list) {
         List<T> result = new ArrayList<>(list.size() / 3 + 1);
         for (int i = 0; i < list.size(); i++) {
